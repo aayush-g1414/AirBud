b24deb612251e660281c99f05d8a8327f846f503
Akshay Belhe <abelhe8900@gmail.com>
2024-01-27 16:08:05 -0600

    ui added

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -1,33 +1,7 @@
-import { StyleSheet, Text, View } from "react-native";
-import { StatusBar } from "expo-status-bar";
-import { Button } from "@repo/ui";
+import AuthNavigation from './src/Screens/AuthNavigation';
 
-export default function Native() {
+export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.header}>Native</Text>
-      <Button
-        onClick={() => {
-          console.log("Pressed!");
-          alert("Pressed!");
-        }}
-        text="Boop"
-      />
-      <StatusBar style="auto" />
-    </View>
-  );
+    <AuthNavigation />
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  header: {
-    fontWeight: "bold",
-    marginBottom: 20,
-    fontSize: 36,
-  },
-});
---
+++ apps/native/src/database/nameDatabase.ts
@@ -0,0 +1,5 @@
+const nameDatabase = {
+    name: "Akshay",
+}
+
+export default nameDatabase;
